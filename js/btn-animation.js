e6c09a834d266d6debae3db9a0981fb39feef1c5
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2022-07-20 21:28:06 -0300

    Update script button-animation.js

--- js/btn-animation.js
+++ js/btn-animation.js
@@ -1,34 +1,30 @@
 const btn = document.getElementsByClassName("content__button");
-var pos = 0;
-var i = 0;
-var max=50;
-var inter = setInterval(move, 20)
 
-var pY = 10;        // Valor inicil del pading en el eje Y
-var px = 25;        // Valor inicil del pading en el eje X
-var mB = 30;        // Valor inicil del margin-bottom
-var fS = 15;        // Valor inicial del font-size
+var pos = 0;    // Posicion en las iteraciones
+var i   = 0;    // Cambio de animacion 1 agrandar escala
+
+// Animacion
+var fps            = 60;   // Cantidad de cambios por segundo
+var secondDuration = 1.5;  // La duracion de cada animacion en segundos
+var changeMax      = 0.2;  // Cambio maximo de la propiedad
+var initScale      = 1;    // Escala inicial
+
+var change  = changeMax/(secondDuration*fps);      // Cuanto de ese cambio se va a aplicar por cada iteracion
+var refresh = (1000)/fps;                          // Cada cuanto se va a actualizar
+var inter   = setInterval(move, refresh)
+
 
 function move(){
-    if((pos >=0) && (pos < max) && (i==0))
+    if((pos >= 0) && (i == 0))
     {
         pos += 1;
-        btn[0].style.padding = `${pY+=0.1}px ${px+=0.1}px `;
-        btn[0].style.fontSize = `${px+=0.01}px `;
-        btn[0].style.marginBottom = `${mB-=0.2}px `;
+        btn[0].style.webkitTransform = `scale(${initScale+=change})`;
+        if(pos>=(secondDuration*fps)){i=1}
 
-        if(pos>=max){
-            i=1;
-        }
-    }else if((pos >0) && (pos <= max) && (i==1))
+    }else if((pos > 0) && (i == 1))
     {
         pos += -1;
-        btn[0].style.padding = `${pY-=0.1}px ${px-=0.1}px `;
-        btn[0].style.fontSize = `${px-=0.01}px `;
-        btn[0].style.marginBottom = `${mB+=0.2}px `;
-
-        if(pos==0){
-            i=0;
-        }
+        btn[0].style.webkitTransform = `scale(${initScale-=change})`;
+        if(pos==0){i=0}
     }
 }
